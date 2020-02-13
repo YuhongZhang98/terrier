4424c63c9dc7225edffa995d40827181fe58a689
YuhongZhang98 <danielzhangyh@gmail.com>
2020-02-13 13:11:38 -0500

    delete end latch

--- src/storage/data_table.cpp
+++ src/storage/data_table.cpp
@@ -72,7 +72,7 @@ DataTable::SlotIterator &DataTable::SlotIterator::operator++() {
 }
 
 DataTable::SlotIterator DataTable::end() const {  // NOLINT for STL name compability
-  common::SpinLatch::ScopedSpinLatch guard(&blocks_latch_);
+//  common::SpinLatch::ScopedSpinLatch guard(&blocks_latch_);
   // TODO(Tianyu): Need to look in detail at how this interacts with compaction when that gets in.
 
   // The end iterator could either point to an unfilled slot in a block, or point to nothing if every block in the
